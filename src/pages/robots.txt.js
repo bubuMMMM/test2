a2b29d56b085f4d271bbf5ba76d5bb37f099415d
bubuMMMM <135442447+bubuMMMM@users.noreply.github.com>
2026-08-07 00:20:07 +0200

    Stage F3CK v2 product assets

--- src/pages/robots.txt.js
+++
@@ -1,5 +0,0 @@
-import { canonical } from '../lib/site.js';
-export const prerender = false;
-export function GET() {
-  return new Response(`User-agent: *\nAllow: /\n\nSitemap: ${canonical('/sitemap.xml')}\n`, { headers: { 'content-type': 'text/plain; charset=utf-8' } });
-}
